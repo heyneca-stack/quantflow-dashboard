a4b49f1d0d4e8ffc763bb49b33edeec012dcf430
heyneca-stack <heyneca@gmail.com>
2026-09-13 09:31:51 +0200

    Create universe.py

---
+++ universe.py
@@ -0,0 +1,24 @@
+{
+  "universes": {
+    "dow": true,
+    "nasdaq100": true,
+    "sp500": true,
+    "russell2000_small_cap": false
+  },
+  "sectors": {
+    "Energie": 1.0,
+    "Grundstoffe": 1.0,
+    "Industrie": 1.0,
+    "Nicht-Basiskonsumgüter": 1.0,
+    "Basiskonsumgüter": 1.0,
+    "Gesundheitswesen": 1.0,
+    "Finanzwerte": 1.0,
+    "Informationstechnologie": 1.0,
+    "Kommunikationsdienste": 1.0,
+    "Versorger": 1.0,
+    "Immobilien": 1.0,
+    "Sektor unbekannt": 1.0,
+    "Krypto/Digital-Asset": 0.0
+  },
+  "max_basket_size": 180
+}
